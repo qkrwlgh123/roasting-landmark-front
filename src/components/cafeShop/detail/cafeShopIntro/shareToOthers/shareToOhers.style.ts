4a4e880c2bd533a5b78669399b8892c971b2ab3b
qkrwlgh123 <jiho1024@naver.com>
2023-10-04 16:51:06 +0900

    [feat] 카카오톡, 링크 공유하기 기능 구현

--- src/components/cafeShop/detail/cafeShopIntro/shareToOthers/shareToOhers.style.ts
+++ src/components/cafeShop/detail/cafeShopIntro/shareToOthers/shareToOhers.style.ts
@@ -1,10 +1,79 @@
 import styled from 'styled-components';
 
-const ShareToOthersBox = styled.div`
-  position: absolute;
-  background-color: white;
-  top: 45%;
-  left: 45%;
+const ShareToOthersBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 2;
 `;
 
-export default { ShareToOthersBox };
+const ContentsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #383b40;
+  align-items: center;
+  position: absolute;
+  padding: 20px;
+  background-color: #ffffff;
+  width: 20%;
+  @media (max-width: 900px) {
+    width: 60%;
+  }
+  height: 25%; /* 중앙 배치 */
+  /* top, bottom, left, right 는 브라우저 기준으로 작동한다. */
+  /* translate는 본인의 크기 기준으로 작동한다. */
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  /* 모달창 디자인 */
+  box-shadow: 0px 4px 16px 0px rgba(0, 0, 0, 0.08);
+  border-radius: 8px;
+`;
+
+const TitleBox = styled.div`
+  margin: 20px 0 30px 0;
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 70%;
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+  font-weight: 300;
+  align-items: center;
+  font-size: 15px;
+  img {
+    width: 70px;
+    height: 70px;
+  }
+`;
+
+const LinkLogoBox = styled.div`
+  width: 70px;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #c4c0bb;
+  border-radius: 10px;
+`;
+
+export default {
+  ShareToOthersBackground,
+  ContentsContainer,
+  TitleBox,
+  ButtonsContainer,
+  ButtonBox,
+  LinkLogoBox,
+};
--- src/components/cafeShop/detail/cafeShopIntro/shareToOthers/shareToOthers.tsx
+++ src/components/cafeShop/detail/cafeShopIntro/shareToOthers/shareToOthers.tsx
@@ -1,20 +1,79 @@
+import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Style from './shareToOhers.style';
+import CloseIcon from '../../../../commonModal/closeIcon/closeIcon';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
-const ShareToOthers = () => {
-  const copyCurrentUrl = () => {
+const ShareToOthers = ({
+  introInfo,
+  handleSwitchShareModel,
+}: {
+  introInfo: {
+    shopId: number;
+    shopName: string;
+    rate: number;
+    participants: number;
+    description: string;
+    keywords: string[];
+  };
+  handleSwitchShareModel: () => void;
+}) => {
+  // 링크 복사
+  const handleCopyCurrentUrl = () => {
     navigator.clipboard.writeText(window.location.href).then(() => {
       alert('링크를 복사했습니다.');
     });
   };
+
+  // 카카오톡으로 공유하기
+  const handleShareByKakao = () => {
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+      if (!kakao.isInitialized()) {
+        kakao.init(process.env.REACT_APP_KAKAO_JSKEY);
+      }
+    }
+
+    window.Kakao.Link.sendCustom({
+      templateId: 99074,
+      templateArgs: {
+        shopId: introInfo.shopId,
+        shopName: introInfo.shopName,
+      },
+    });
+  };
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://developers.kakao.com/sdk/js/kakao.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
   return (
-    <Style.ShareToOthersBox>
-      <div onClick={copyCurrentUrl}>
-        <button>링크 복사</button>
-      </div>
-      <div>
-        <button>카카오톡으로 공유하기</button>
-      </div>
-    </Style.ShareToOthersBox>
+    <Style.ShareToOthersBackground>
+      <Style.ContentsContainer>
+        <CloseIcon onClickFunc={handleSwitchShareModel} />
+        <Style.TitleBox>
+          <span>공유하기</span>
+        </Style.TitleBox>
+        <Style.ButtonsContainer>
+          <Style.ButtonBox onClick={handleCopyCurrentUrl}>
+            <Style.LinkLogoBox>
+              <FontAwesomeIcon color="#ffffff" size="xl" icon={faLink} />
+            </Style.LinkLogoBox>
+            <span>링크</span>
+          </Style.ButtonBox>
+          <Style.ButtonBox onClick={handleShareByKakao}>
+            <img
+              src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
+              alt="kakao_logo"
+            />
+            <span>카카오톡</span>
+          </Style.ButtonBox>
+        </Style.ButtonsContainer>
+      </Style.ContentsContainer>
+    </Style.ShareToOthersBackground>
   );
 };
 
